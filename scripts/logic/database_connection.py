856e0b97821a70c14dd1602789739ca65b98138c
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-20 15:48:43 +0100

    chore : delete unused file, add docstring

--- scripts/logic/database_connection.py
+++ scripts/logic/database_connection.py
@@ -2,7 +2,9 @@ import mysql.connector
 
 DB_NAME = "budget_buddy"
 
+
 def get_connection():
+    """Return a new MySQL connection to the budget_buddy database."""
     return mysql.connector.connect(
         host="127.0.0.1",
         user="budget_buddy_test",
